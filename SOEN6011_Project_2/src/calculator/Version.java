bf6b5d135075b83a0d25bf11680c894d0646d128
Muqaddaspreet Singh <54834880+Muqaddaspreet@users.noreply.github.com>
2024-08-05 19:34:00 -0400

    Created the project Scientific Calculator

---
+++ SOEN6011_Project_2/src/calculator/Version.java
@@ -0,0 +1,30 @@
+package calculator;
+
+/**
+ * This class holds the version information of the application.
+ * The version follows Semantic Versioning (https://semver.org/).
+ */
+public final class Version {
+    /** The major version number. */
+    public static final int MAJOR = 2;
+    /** The minor version number. */
+    public static final int MINOR = 1;
+    /** The patch version number. */
+    public static final int PATCH = 2;
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private Version() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    /**
+     * Returns the version string in the format MAJOR.MINOR.PATCH.
+     *
+     * @return the version string.
+     */
+    public static String getVersion() {
+        return MAJOR + "." + MINOR + "." + PATCH;
+    }
+}
